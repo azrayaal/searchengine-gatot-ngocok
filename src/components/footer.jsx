f02c60eae2a33b2c4bb9cf0b417068ddf39a7b98
azra <90897554+azrayaal@users.noreply.github.com>
2022-09-28 14:14:23 +0700

    update footer link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Futer() {
   return (
     <div className="text-center text-white text-sm ">
-      © 2022 Gatot Ngocok - Project Gabut by <span className="text-sky-600">azrayaal</span>
+      © 2022 Gatot Ngocok - Project Gabut by <a href="https://azrayaaal.herokuapp.com" className="text-sky-600" style={{textDecoration: 'none'}}>azrayaal</a>
     </div>
   );
 }
